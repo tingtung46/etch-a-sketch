bff69f8a67b236e7063af39e3a308416a207c25e
Rizma Ernawanti <132725436+tingtung46@users.noreply.github.com>
2023-10-24 22:07:14 +0700

    Create dynamical grid

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function createDivs (numberofDivs) {
     const container = document.querySelector('.container');
 
-    for (i = 0; i < numberofDivs; i++) {
+    for (i = 0; i < (numberofDivs * numberofDivs); i++) {
         const newDiv = document.createElement('div');
         newDiv.classList.add('item');
+
+        const boxSize = 500 / numberofDivs;
+        newDiv.style.width = boxSize + 'px';
+        newDiv.style.height = boxSize + 'px';
+
         container.appendChild(newDiv);
     };
 };
 
-createDivs(256);
+createDivs(40);
